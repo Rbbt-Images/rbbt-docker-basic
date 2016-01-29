b4e1b3b48a5af2a94bef186a8f8c5e1ca981462e
Miguel Vazquez <miguel.vazquez@fdi.ucm.es>
2016-01-29 17:31:42 +0100

    Improved provision.sh layout

--- provision.sh
+++ provision.sh
@@ -1,21 +1,24 @@
-cat "$0"
-echo "Running provisioning"
-echo
-
-# BASE SYSTEM
-echo "1. Provisioning base system"
-
-
-# BASE SYSTEM
-echo "2. Setting up ruby"
-
-
-# BASE SYSTEM
-echo "2. Setting up gems"
 #!/bin/bash -x
 
-# Ruby gems and Rbbt
-# -------------------------
+echo "RUNNING PROVISION"
+echo
+echo "CMD: git/rbbt-image/bin/build_rbbt_provision_sh.rb -ss -sr -sb"
+
+echo "1. Provisioning base system"
+echo
+echo SKIPPED
+
+echo "2. Setting up ruby"
+echo
+echo SKIPPED
+
+echo "3. Setting up gems"
+echo
+#!/bin/bash -x
+
+# RUBY GEMS and RBBT
+# =================
+
 export REALLY_GEM_UPDATE_SYSTEM=true
 env REALLY_GEM_UPDATE_SYSTEM=true gem update --system
 gem install --force ZenTest
@@ -42,8 +45,8 @@ gem install /tmp/lockfile-2.1.4.gem
 
 
 
-
-
+echo "4. Configuring user"
+echo
 ####################
 # USER CONFIGURATION
 
@@ -54,19 +57,22 @@ else
   home_dir='/home/rbbt'
 fi
 
-user_script=$home_dir/.rbbt/bin/provision
+user_script=$home_dir/.rbbt/bin/config_user
 mkdir -p $(dirname $user_script)
 chown -R rbbt /home/rbbt/.rbbt/
+
+
+# set user configuration script
 cat > $user_script <<'EUSER'
 
 . /etc/profile
 
-echo "2.1. Custom variables"
+echo "4.1. Loading custom variables"
 export RBBT_LOG="0"
 export BOOTSTRAP_WORKFLOWS="Enrichment Translation Sequence MutationEnrichment"
 export REMOTE_RESOURCES="KEGG"
 
-echo "2.2. Default variables"
+echo "4.2. Loading default variables"
 #!/bin/bash -x
 
 test -z ${RBBT_SERVER+x}           && RBBT_SERVER=http://rbbt.bioinfo.cnio.es/ 
@@ -87,11 +93,11 @@ test -z ${RBBT_LOG+x}  && RBBT_LOG="LOW"
 
 
 
-echo "2.3. Configuring rbbt"
+echo "4.3. Configuring rbbt"
 #!/bin/bash -x
 
-# GENERAL
-# -------
+# USER RBBT CONFIG
+# ================
 
 # File servers: to speed up the production of some resources
 for resource in $REMOTE_RESOURCES; do
@@ -105,42 +111,21 @@ for workflow in $REMOTE_WORKFLOWS; do
     rbbt workflow remote add $workflow $RBBT_WORKFLOW_SERVER
 done
 
-
-exit
-
-echo "2.4. Bootstrap system"
-#!/bin/bash -x
-
-# APP
-# ---
-
-for workflow in $WORKFLOWS; do
-    rbbt workflow install $workflow 
-done
-
-export RBBT_WORKFLOW_AUTOINSTALL
-export RBBT_LOG
-
-for workflow in $BOOTSTRAP_WORKFLOWS; do
-    echo "Bootstrapping $workflow on $BOOTSTRAP_CPUS CPUs"
-    rbbt workflow cmd $workflow bootstrap $BOOTSTRAP_CPUS
-done
-
-
 EUSER
-####################
-echo "2. Running user configuration as 'rbbt'"
+
+echo "4.4. Running user configuration as 'rbbt'"
 chown rbbt $user_script;
 su -l -c "bash $user_script" rbbt
 
-# CLEAN-UP
+echo "5. Bootstrapping workflows as 'rbbt'"
+echo
+echo SKIPPED
+
+# CODA
+# ====
 
 RUN apt-get clean
 RUN rm -rf /var/lib/apt/lists/* /tmp/* /var/tmp/*
 
-# DONE
 echo
 echo "Installation done."
-
-#--------------------------------------------------------
-
